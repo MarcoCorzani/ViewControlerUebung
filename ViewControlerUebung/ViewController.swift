83e8dfed8848f5718e6b320653915d61dc86f468
Marco <Marco@Marcos-iMac.fritz.box>
2015-01-07 20:30:50 +0100

    Anfang

--- ViewControlerUebung/ViewController.swift
+++ ViewControlerUebung/ViewController.swift
@@ -10,9 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    var colorView: UIView!
+   
+    
+    @IBOutlet weak var colorSelect: UISegmentedControl!
+    
+    
+    @IBAction func segmentButton(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        
+        case 0: colorView?.backgroundColor = UIColor.redColor()
+        
+        case 1: colorView?.backgroundColor = UIColor.greenColor()
+        
+        case 2: colorView?.backgroundColor = UIColor.blueColor()
+        
+        default:
+            println("Error: segment \(sender.selectedSegmentIndex) does not exist")
+        }
+    
+    
+    }
+    
+    
+    @IBAction func showHide(sender: UIButton) {
+        if sender.titleLabel?.text == "Show" {
+            sender.setTitle("Hide", forState: UIControlState.Normal)
+            colorSelect.selectedSegmentIndex = 0
+            let colorViewFrame = CGRectMake(120, 300, 140, 140)
+            colorView = UIView(frame: colorViewFrame)
+            colorView.backgroundColor = UIColor.redColor()
+            view.addSubview(colorView)
+        } else {
+            sender.setTitle("Show", forState: UIControlState.Normal)
+            colorView.removeFromSuperview()
+            colorView = nil
+        
+        }
+    
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
